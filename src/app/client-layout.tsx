3b3e2537b3bd3b3a0f89972caed5162b6f8f760c
Anshulllg <anshul20361@iiitd.ac.in>
2025-01-21 11:54:43 +0530

    fixed layout

---
+++ src/app/client-layout.tsx
@@ -0,0 +1,29 @@
+// client-layout.tsx
+'use client';
+
+import { useLayoutEffect, useState } from "react";
+import LoadingScreen from "@/components/Nav/LoadingScreen";
+import CustomCursor from "@/components/Nav/CustomCursor";
+
+export default function ClientLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useLayoutEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <>
+      <CustomCursor />
+      {isLoading ? <LoadingScreen /> : children}
+    </>
+  );
+}
